84013f0d4fecb4f5ec13d34f33ec12556e40b76c
TShipley <taneymail@gmail.com>
2020-01-26 23:52:48 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,14 @@
 def unsafe?(speed)
-
+  if speed < 40 || > 60
+    return TRUE
 end
 
 
 
 def not_safe?(speed)
-	
+  speed < 40 ? "unsafe" : "safe" 	
+  ||
+  speed > 90 ? "safe" : "unsafe"
 end
 	
 
